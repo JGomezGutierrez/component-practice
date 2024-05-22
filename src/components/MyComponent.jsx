03a862cd0cc26c014054b7ad091e3978417545d2
Juliet Gomez <yuligomg@gmail.com>
2024-05-21 19:01:40 -0500

    My first Component

---
+++ src/components/MyComponent.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export const MyComponent = () => {
+  const name = "Inés";
+  const repository = "https://github.com/inesmariao/component-practice.git/"; 
+ 
+  const student = {
+    name: "Juliet",
+    LastName: "Gomez",
+    mobile: "3145494820",
+    LinkedinProfile: "https:"
+  }
+ 
+  return (
+    <div>
+        <hr/>
+        <p>Este es mi primer componente</p>
+        <h2>Temas de React</h2>
+        <ul>
+            <li>Componentes</li>
+            <li>Eventos</li>
+            <li>Estados - Hook</li>
+            <li>Comunicacion</li>
+        </ul>
+        <hr />
+        <h2>Datos del Docente</h2>
+        <p>Nombre:{ name }</p>
+        <p>Repositorio:{ repository } </p>
+        <hr/>
+        <h2>Datos del estudiante</h2>
+        <p>Nombre: { JSON.stringify ( student )}</p> 
+        <p>Nombre: {student.name} </p>
+        <p>Apellido: {student.LastName} </p>
+        <p>Celular: {student.mobile} </p>
+        <p>Perfil de Linkedin: { student.LinkedinProfile } </p>
+    </div>
+  )
+}
+
+export default MyComponent
+
